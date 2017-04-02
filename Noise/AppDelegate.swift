57f574de91eb55561d9c634b607d875f9e882bf2
Jeff Larson <thejefflarson@gmail.com>
2017-04-01 21:14:19 -0400

    this->sky

---
+++ Noise/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Noise
+//
+//  Created by Jeff Larson on 4/1/17.
+//  Copyright © 2017 Jeff Larson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
