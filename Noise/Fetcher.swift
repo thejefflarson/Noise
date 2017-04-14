0f50288e70bfb8b8a51d511efb68b65ab78d8112
Jeff Larson <thejefflarson@gmail.com>
2017-04-13 23:45:59 -0400

    up limit

--- Noise/Fetcher.swift
+++ Noise/Fetcher.swift
@@ -155,7 +155,7 @@ class Fetcher : NSObject {
     private let maxSize = 1000000
     private func go() {
         // WKWebView is very very leaky let's let 10 requests go by before we kill the subprocess.
-        if self.count % 10 == 0 {
+        if self.count % 50 == 0 {
             self.loader = Loader()
         }
         let prob = Double(arc4random_uniform(UInt32(maxSize))) / Double(maxSize)
